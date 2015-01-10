3c4a2fd3f32cc54c94699bbc461439d4e1774ed6
Esteve Fernandez <esteve@osrfoundation.org>
2015-01-09 17:42:25 -0800

    Removed timeouts

--- ros_middleware_connext_xtypes_dynamic_cpp/src/functions.cpp
+++ ros_middleware_connext_xtypes_dynamic_cpp/src/functions.cpp
@@ -835,7 +835,7 @@ void send_request(
 }
 
 bool receive_response(
-  const ClientHandle& client_handle, void * ros_response, long timeout)
+  const ClientHandle& client_handle, void * ros_response)
 {
     return false;
 }
