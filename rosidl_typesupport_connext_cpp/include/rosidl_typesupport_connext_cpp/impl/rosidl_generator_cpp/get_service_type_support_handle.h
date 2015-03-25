79b273ad4199a9449c8a65615d90d989778c065f
William Woodall <william@osrfoundation.org>
2015-03-25 04:03:50 -0700

    consolidate implementation specific headers
    Each implementation provides a custom version of
    the get_type_support_handle function and
    previously this customized header existed in both
    the rmw_connext_cpp and
    rmw_connext_xtypes_dynamic_cpp packages. Now it
    is only in the rosidl_typesupport_connext_cpp
    package.

--- rosidl_typesupport_connext_cpp/include/rosidl_typesupport_connext_cpp/impl/rosidl_generator_cpp/get_service_type_support_handle.h
+++ rosidl_typesupport_connext_cpp/include/rosidl_typesupport_connext_cpp/impl/rosidl_generator_cpp/get_service_type_support_handle.h
@@ -1,14 +1,14 @@
 #ifndef __rmw__get_service_type_support_handle__h__
 #define __rmw__get_service_type_support_handle__h__
 
-#include "rosidl_typesupport_connext_cpp/ServiceTypeSupport.h"
+#include "rosidl_typesupport_opensplice_cpp/ServiceTypeSupport.h"
 
 namespace rmw
 {
 template<typename T>
 const rosidl_service_type_support_t * get_service_type_support_handle()
 {
-  return rmw_connext_cpp::get_service_type_support_handle<T>();
+  return rmw_opensplice_cpp::get_service_type_support_handle<T>();
 }
 
 }  // namespace rmw
