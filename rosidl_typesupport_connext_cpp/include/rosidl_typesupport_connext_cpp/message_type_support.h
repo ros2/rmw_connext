e7c392fb3197c8e8dbec6a9ec752745949e00659
Dirk Thomas <dthomas@osrfoundation.org>
2015-04-07 10:59:02 -0700

    code style only

--- rosidl_typesupport_connext_cpp/include/rosidl_typesupport_connext_cpp/message_type_support.h
+++ rosidl_typesupport_connext_cpp/include/rosidl_typesupport_connext_cpp/message_type_support.h
@@ -22,11 +22,11 @@ typedef struct message_type_support_callbacks_t
   const char * package_name;
   const char * message_name;
   // Function to register type with given dds_participant
-  void (*register_type)(void * dds_participant, const char * type_name);
+  void (* register_type)(void * dds_participant, const char * type_name);
   // Function to publish a ROS message with a given DDS data_writer
-  void (*publish)(void * dds_data_writer, const void * ros_message);
+  void (* publish)(void * dds_data_writer, const void * ros_message);
   // Function to take a ROS message from a dds data reader
-  bool (*take)(void * dds_data_reader, void * ros_message);
+  bool (* take)(void * dds_data_reader, void * ros_message);
 } message_type_support_callbacks_t;
 
 #endif  /* ROSIDL_TYPESUPPORT_CONNEXT_CPP_ROSIDL_TYPESUPPORT_CONNEXT_CPP_MESSAGE_TYPE_SUPPORT_H_ */
--- rosidl_typesupport_connext_cpp/include/rosidl_typesupport_connext_cpp/service_type_support.h
+++ rosidl_typesupport_connext_cpp/include/rosidl_typesupport_connext_cpp/service_type_support.h
@@ -24,19 +24,23 @@ typedef struct service_type_support_callbacks_t
   const char * package_name;
   const char * service_name;
   // Function to create a requester
-  void* (*create_requester)(void * participant, const char * service_name,
-                            void **reader);
+  void * (*create_requester)(
+    void * participant, const char * service_name,
+    void ** reader);
   // Function to create a replier
-  void* (*create_replier)(void * participant, const char * service_name,
-                          void **reader);
+  void * (*create_replier)(
+    void * participant, const char * service_name,
+    void ** reader);
   // Function to send ROS requests
-  int64_t (*send_request)(void * requester, const void * ros_request);
+  int64_t (* send_request)(void * requester, const void * ros_request);
   // Function to read a ROS request from the wire
-  bool (*take_request)(void * replier, void * ros_request_header, void * ros_request);
+  bool (* take_request)(void * replier, void * ros_request_header, void * ros_request);
   // Function to send ROS responses
-  void (*send_response)(void * replier, const void * ros_request_header, const void * ros_response);
+  void (* send_response)(
+    void * replier, const void * ros_request_header,
+    const void * ros_response);
   // Function to read a ROS response from the wire
-  bool (*take_response)(void * requester, void * ros_request_header, void * ros_response);
+  bool (* take_response)(void * requester, void * ros_request_header, void * ros_response);
 } service_type_support_callbacks_t;
 
 #endif  /* ROSIDL_TYPESUPPORT_CONNEXT_CPP_ROSIDL_TYPESUPPORT_CONNEXT_CPP_SERVICE_TYPE_SUPPORT_H_ */
