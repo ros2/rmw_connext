a1516c1836f97c5fe0935cd8e77cc3301e9cdae6
Dirk Thomas <dthomas@osrfoundation.org>
2015-06-21 18:29:48 -0700

    fix using same type code multiple times, check for all error cases explicitly

--- rosidl_typesupport_connext_cpp/include/rosidl_typesupport_connext_cpp/message_type_support.h
+++ rosidl_typesupport_connext_cpp/include/rosidl_typesupport_connext_cpp/message_type_support.h
@@ -22,9 +22,9 @@ typedef struct message_type_support_callbacks_t
   const char * package_name;
   const char * message_name;
   // Function to register type with given dds_participant
-  void (* register_type)(void * dds_participant, const char * type_name);
+  bool (* register_type)(void * dds_participant, const char * type_name);
   // Function to publish a ROS message with a given DDS data_writer
-  void (* publish)(void * dds_data_writer, const void * ros_message);
+  bool (* publish)(void * dds_data_writer, const void * ros_message);
   // Function to take a ROS message from a dds data reader
   bool (* take)(void * dds_data_reader, void * ros_message);
 } message_type_support_callbacks_t;
