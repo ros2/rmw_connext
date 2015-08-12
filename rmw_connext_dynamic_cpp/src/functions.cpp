a5efc5adec945be57d578f3691048918eb42bcd2
Jackie Kay <jackie@osrfoundation.org>
2015-08-12 10:03:03 -0700

    More descriptive error messages for attach_condition

--- rmw_connext_dynamic_cpp/src/functions.cpp
+++ rmw_connext_dynamic_cpp/src/functions.cpp
@@ -153,6 +153,21 @@ rmw_get_implementation_identifier()
   return rti_connext_dynamic_identifier;
 }
 
+rmw_ret_t check_attach_condition_error(DDS::ReturnCode_t retcode)
+{
+  if (retcode == DDS_RETCODE_OK) {
+    return RMW_RET_OK;
+  }
+  if (retcode == DDS_RETCODE_OUT_OF_RESOURCES) {
+    RMW_SET_ERROR_MSG("failed to attach condition to waitset: out of resources");
+  } else if (retcode == DDS_RETCODE_BAD_PARAMETER) {
+    RMW_SET_ERROR_MSG("failed to attach condition to waitset: condition pointer was invalid");
+  } else {
+    RMW_SET_ERROR_MSG("failed to attach condition to waitset");
+  }
+  return RMW_RET_ERROR;
+}
+
 rmw_ret_t
 rmw_init()
 {
@@ -2475,10 +2490,10 @@ rmw_wait(
       RMW_SET_ERROR_MSG("read condition handle is null");
       return RMW_RET_ERROR;
     }
-    DDS_ReturnCode_t status = waitset.attach_condition(read_condition);
-    if (status != DDS_RETCODE_OK) {
-      RMW_SET_ERROR_MSG("failed to attach condition");
-      return RMW_RET_ERROR;
+    rmw_ret_t status = check_attach_condition_error(
+      waitset.attach_condition(read_condition));
+    if (status != RMW_RET_OK) {
+      return status;
     }
   }
 
@@ -2490,10 +2505,10 @@ rmw_wait(
       RMW_SET_ERROR_MSG("guard condition handle is null");
       return RMW_RET_ERROR;
     }
-    DDS_ReturnCode_t status = waitset.attach_condition(guard_condition);
-    if (status != DDS_RETCODE_OK) {
-      RMW_SET_ERROR_MSG("failed to attach condition");
-      return RMW_RET_ERROR;
+    rmw_ret_t status = check_attach_condition_error(
+      waitset.attach_condition(guard_condition));
+    if (status != RMW_RET_OK) {
+      return status;
     }
   }
 
@@ -2520,10 +2535,10 @@ rmw_wait(
       RMW_SET_ERROR_MSG("failed to set enabled statuses");
       return RMW_RET_ERROR;
     }
-    status = waitset.attach_condition(condition);
-    if (status != DDS_RETCODE_OK) {
-      RMW_SET_ERROR_MSG("failed to attach condition");
-      return RMW_RET_ERROR;
+    rmw_ret_t status = check_attach_condition_error(
+      waitset.attach_condition(condition));
+    if (status != RMW_RET_OK) {
+      return status;
     }
   }
 
@@ -2550,10 +2565,10 @@ rmw_wait(
       RMW_SET_ERROR_MSG("failed to set enabled statuses");
       return RMW_RET_ERROR;
     }
-    status = waitset.attach_condition(condition);
-    if (status != DDS_RETCODE_OK) {
-      RMW_SET_ERROR_MSG("failed to attach condition");
-      return RMW_RET_ERROR;
+    rmw_ret_t status = check_attach_condition_error(
+      waitset.attach_condition(condition));
+    if (status != RMW_RET_OK) {
+      return status;
     }
   }
 
