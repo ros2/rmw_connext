035ecab91aac0a0faa0a54b669ce9476ab1304c7
Dirk Thomas <dthomas@osrfoundation.org>
2016-03-09 12:47:55 -0800

    fix stric-aliasing warning

--- rosidl_typesupport_connext_cpp/cmake/rosidl_typesupport_connext_cpp_generate_interfaces.cmake
+++ rosidl_typesupport_connext_cpp/cmake/rosidl_typesupport_connext_cpp_generate_interfaces.cmake
@@ -82,6 +82,7 @@ if(NOT WIN32)
     )
   elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
     set(_connext_compile_flags
+      "-Wno-strict-aliasing"
       "-Wno-unused-but-set-variable"
       "-Wno-unused-parameter"
       "-Wno-unused-variable"
