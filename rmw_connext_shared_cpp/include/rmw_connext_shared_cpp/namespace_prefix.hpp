515b1603172f0c88a79d3f063f6444000e4101e6
Karsten Knese <karsten.knese@googlemail.com>
2017-07-19 08:49:05 -0700

    cleanup new connext_shared header files

--- rmw_connext_shared_cpp/include/rmw_connext_shared_cpp/namespace_prefix.hpp
+++ rmw_connext_shared_cpp/include/rmw_connext_shared_cpp/namespace_prefix.hpp
@@ -12,17 +12,20 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef RMW_CONNEXT_SHARED_CPP__TOPIC_NAMES_AND_TYPES_HPP_
-#define RMW_CONNEXT_SHARED_CPP__TOPIC_NAMES_AND_TYPES_HPP_
+#ifndef RMW_CONNEXT_SHARED_CPP__NAMESPACE_PREFIX_HPP_
+#define RMW_CONNEXT_SHARED_CPP__NAMESPACE_PREFIX_HPP_
 
-#include "rmw/get_topic_names_and_types.h"
+#include <string>
+#include <vector>
 
-RMW_CONNEXT_SHARED_CPP_PUBLIC
-rmw_ret_t
-get_topic_names_and_types(const char * implementation_identifier,
-  const rmw_node_t * node,
-  rcutils_allocator_t * allocator,
-  bool no_demangle,
-  rmw_names_and_types_t * topic_names_and_types);
+extern const char * const ros_topic_prefix;
+extern const char * const ros_service_requester_prefix;
+extern const char * const ros_service_response_prefix;
 
-#endif  // RMW_CONNEXT_SHARED_CPP__TOPIC_NAMES_AND_TYPES_HPP_
+extern std::vector<std::string> _ros_prefixes;
+
+/// Return the ROS specific prefix if it exists, otherwise "".
+std::string
+_get_ros_prefix_if_exists(const std::string & topic_name);
+
+#endif  // RMW_CONNEXT_SHARED_CPP__NAMESPACE_PREFIX_HPP_
