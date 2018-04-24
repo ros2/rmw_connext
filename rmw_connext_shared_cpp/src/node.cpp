e328c0a9efab45281bd30eb787cb623604aab402
rohit <rohitsalem@gmail.com>
2018-04-24 16:19:03 -0700

    Added detailed explanation for the changed property max length

--- rmw_connext_shared_cpp/src/node.cpp
+++ rmw_connext_shared_cpp/src/node.cpp
@@ -70,13 +70,16 @@ create_node(
     participant_qos.user_data.value[sizeof(prefix) - 1 + name_length] = ';';
   }
 
-  // Setting the contentfilter_property_max_length to 1024
-  // The limit 1024 is accomodate sum of the lengths of
-  // contentFilteredTopicName (max length 256)
-  // relatedTopicName (max length 256)
-  // filterClassName (max length 256)
-  // filterName (DDSSQL)
-  // filterExpression
+
+  // Accorinrding to the RTPS spec, ContentFilterProperty_t has the following fields:
+  // -contentFilteredTopicName (max length 256)
+  // -relatedTopicName (max length 256)
+  // -filterClassName (max length 256)
+  // -filterName (DDSSQL)
+  // -filterExpression
+  // In Connext, contentfilter_property_max_length is sum of lengths of all these fields,
+  // which by default is 256.
+  // So we set the limit to 1024, to accomodate the complete topic name with namespaces.
   participant_qos.resource_limits.contentfilter_property_max_length = 1024;
 
   // forces local traffic to be sent over loopback,
