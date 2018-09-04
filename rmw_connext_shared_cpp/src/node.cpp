f73af27af2768c52f81209b3067865df2bf1cd4c
Michael Carroll <michael@openrobotics.org>
2018-09-04 15:08:53 -0500

    Check snprintf output.

--- rmw_connext_shared_cpp/src/node.cpp
+++ rmw_connext_shared_cpp/src/node.cpp
@@ -63,10 +63,10 @@ create_node(
     return NULL;
   }
 
-  size_t written =
+  int written =
     snprintf(reinterpret_cast<char *>(participant_qos.user_data.value.get_contiguous_buffer()),
       length, "name=%s;namespace=%s;", name, namespace_);
-  if (written < 0) {
+  if (written < 0 || written > static_cast<int>(length) - 1) {
     RMW_SET_ERROR_MSG("failed to populate user_data buffer");
     return NULL;
   }
