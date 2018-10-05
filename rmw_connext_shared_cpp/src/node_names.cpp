815b5dc876cbdcf003ce5487cba131d1a9dfa6be
Chris Lalancette <clalancette@gmail.com>
2018-10-05 17:30:07 -0400

    Add semicolons to all RCLCPP and RCUTILS macros. (#304)
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- rmw_connext_shared_cpp/src/node_names.cpp
+++ rmw_connext_shared_cpp/src/node_names.cpp
@@ -134,7 +134,7 @@ fail:
     if (rcutils_ret != RCUTILS_RET_OK) {
       RCUTILS_LOG_ERROR_NAMED(
         "rmw_connext_cpp",
-        "failed to cleanup during error handling: %s", rcutils_get_error_string_safe())
+        "failed to cleanup during error handling: %s", rcutils_get_error_string_safe());
       rcutils_reset_error();
     }
   }
@@ -143,7 +143,7 @@ fail:
     if (rcutils_ret != RCUTILS_RET_OK) {
       RCUTILS_LOG_ERROR_NAMED(
         "rmw_connext_cpp",
-        "failed to cleanup during error handling: %s", rcutils_get_error_string_safe())
+        "failed to cleanup during error handling: %s", rcutils_get_error_string_safe());
       rcutils_reset_error();
     }
   }
