49711047cecffcd7a4c6a7d636cd24286460d8ac
William Woodall <william+github@osrfoundation.org>
2018-11-01 21:08:05 -0500

    use new error handling API from rcutils (#306)
    * use new error handling API from rcutils
    
    Signed-off-by: William Woodall <william@osrfoundation.org>
    
    * use semicolons after macros
    
    Signed-off-by: William Woodall <william@osrfoundation.org>
    Signed-off-by: captaintrunky <captaintrunky@gmail.com>

--- rmw_connext_shared_cpp/src/node_names.cpp
+++ rmw_connext_shared_cpp/src/node_names.cpp
@@ -64,7 +64,7 @@ get_node_names(
   }
   rcutils_ret = rcutils_string_array_init(node_namespaces, length, &allocator);
   if (rcutils_ret != RCUTILS_RET_OK) {
-    RMW_SET_ERROR_MSG(rcutils_get_error_string_safe())
+    RMW_SET_ERROR_MSG(rcutils_get_error_string().str);
     return rmw_convert_rcutils_ret_to_rmw_ret(rcutils_ret);
   }
 
@@ -74,6 +74,7 @@ get_node_names(
     RMW_SET_ERROR_MSG("failed to get default participant qos");
     return RMW_RET_ERROR;
   }
+
 
   // Pre-allocate temporary buffer for all node names
   // Nodes that are not created by ROS2 could provide empty names
@@ -102,6 +103,7 @@ get_node_names(
 
     return RMW_RET_BAD_ALLOC;
   }
+
   node_namespaces->data[0] = rcutils_strdup(node->namespace_, allocator);
   if (!node_namespaces->data[0]) {
     RMW_SET_ERROR_MSG("could not allocate memory for node namespace");
@@ -110,6 +112,12 @@ get_node_names(
   node_namespaces->data[0] = rcutils_strdup(node->namespace_, allocator);
   if (!node_namespaces->data[0]) {
     RMW_SET_ERROR_MSG("could not allocate memory for node namespace")
+    return RMW_RET_BAD_ALLOC;
+  }
+
+  node_names->data[0] = rcutils_strdup(participant_qos.participant_name.name, allocator);
+  if (!node_names->data[0]) {
+    RMW_SET_ERROR_MSG("could not allocate memory for node name");
     return RMW_RET_BAD_ALLOC;
   }
 
@@ -164,13 +172,13 @@ get_node_names(
 
     node_names->data[i] = rcutils_strdup(name.c_str(), allocator);
     if (!node_names->data[i]) {
-      RMW_SET_ERROR_MSG("could not allocate memory for node name")
+      RMW_SET_ERROR_MSG("could not allocate memory for node name");
       goto fail;
     }
 
     node_namespaces->data[i] = rcutils_strdup(namespace_.c_str(), allocator);
     if (!node_namespaces->data[i]) {
-      RMW_SET_ERROR_MSG("could not allocate memory for node namespace")
+      RMW_SET_ERROR_MSG("could not allocate memory for node namespace");
       goto fail;
     }
 
