c7e324cf7649dea1b49ae164eb3b0204d08898fa
Chris Lalancette <clalancette@openrobotics.org>
2018-12-06 20:18:42 +0000

    Minor rearrangement of includes.
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- rmw_connext_shared_cpp/src/names_and_types_helpers.cpp
+++ rmw_connext_shared_cpp/src/names_and_types_helpers.cpp
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <set>
 #include <map>
+#include <set>
 #include <string>
 
 #include "rcutils/error_handling.h"
