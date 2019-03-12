8f3a91e221f4ec55606f4b1fc0824e1d616ce79e
Ross Desmond <rddesmon@amazon.com>
2019-03-12 11:42:43 -0700

    Add rmw_take_event stub
    Edit: Fix handle active event conditions to only set active specific
    events.
    
    @todo: found multiple loops through active conditions which is not necessary in
    wait.hpp, time cost on average O(n), where n is the number of
    conditions. Actual time cost: 4*n. Can be removed and made O(1).
    
    Signed-off-by: Devin Bonnie <dbbonnie@amazon.com>
    Signed-off-by: Burek <burekn@f45c89c6612b.ant.amazon.com>

--- rmw_connext_cpp/src/rmw_event.cpp
+++ rmw_connext_cpp/src/rmw_event.cpp
@@ -2,7 +2,11 @@
 
 #include "rmw/rmw.h"
 #include "rmw_fastrtps_shared_cpp/rmw_common.hpp"
+#include "rmw_fastrtps_shared_cpp/event.hpp"
 #include "rmw_fastrtps_cpp/identifier.hpp"
+
+extern "C"
+{
 /*
  * Take an event from the event handle.
  *
@@ -16,4 +20,12 @@ rmw_ret_t
 rmw_take_event(
   const rmw_event_t * event_handle,
   void * event,
-  bool * taken);
+  bool * taken) {
+	return rmw_fastrtps_shared_cpp::__rmw_take_event(
+		rti_connext_identifier,
+		event_handle,
+		event,
+		taken);
+}
+
+}  // extern "C"
