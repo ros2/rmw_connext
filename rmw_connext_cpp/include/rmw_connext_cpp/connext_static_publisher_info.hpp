0b6447494d45f7891f6d30617a0cff461e82dbf2
Devin Bonnie <dbbonnie@amazon.com>
2019-03-15 11:35:25 -0700

    Implemented the take event method Added handling of the take event in both static publisher and static subscriber
    Signed-off-by: Devin Bonnie <dbbonnie@amazon.com>
    Signed-off-by: Burek <burekn@f45c89c6612b.ant.amazon.com>

--- rmw_connext_cpp/include/rmw_connext_cpp/connext_static_publisher_info.hpp
+++ rmw_connext_cpp/include/rmw_connext_cpp/connext_static_publisher_info.hpp
@@ -25,7 +25,7 @@ class ConnextPublisherListener;
 
 extern "C"
 {
-struct ConnextStaticPublisherInfo
+struct ConnextStaticPublisherInfo : ConnextCustomEventInfo
 {
   DDS::Publisher * dds_publisher_;
   ConnextPublisherListener * listener_;
@@ -53,5 +53,85 @@ public:
 private:
   std::atomic<std::size_t> current_count_;
 };
+
+/**
+ * Remap the specific RTI Connext DDS DataWriter Status to a generic RMW status type.
+ *
+ * @param mask input status mask
+ * @param event
+ */
+inline rmw_ret_code ConnextStaticSubscriberInfo::get_status(
+        const DDS_StatusMask mask,
+        void * event) {
+
+  switch(mask) {
+
+    case DDS_LIVELINESS_LOST:
+
+      LivelinessLostStatus * liveliness_lost = topic_writer_->liveliness_lost_status();
+
+      rmw_liveliness_lost_t rmw_liveliness_lost = new liveliness_lost_t();
+      rmw_liveliness_lost.total_count = liveliness_lost.total_count();
+      rmw_liveliness_lost.total_count_change = liveliness_lost.total_count_change();
+
+      //todo return value?
+      //todo assign to void *?
+      break;
+
+    case DDS_OFFERED_DEADLINE_MISSED:
+
+      OfferedDeadlineMissedStatus * offered_deadline_missed = topic_writer_->offered_deadline_missed_status();
+
+      rmw_offered_deadline_missed_t rmw_offered_deadline_missed = new rmw_offered_deadline_missed_t();
+      rmw_offered_deadline_missed.total_count = offered_deadline_missed->total_count();
+      rmw_offered_deadline_missed.total_count_change = offered_deadline_missed->total_count_change();
+      //TODO
+      //rmw_offered_deadline_missed.last_instance_handle = offered_deadline_missed->last_instance_handle();
+
+      //todo return value?
+      //todo assign to void *?
+      break;
+
+    case DDS_OFFERED_INCOMPATIBLE_QOS:
+
+      OfferedIncompatibleQosStatus * offered_incompatible_status = topic_writer_->offered_incompatible_status();
+
+      rmw_offered_incompatible_qos_t rmw_offered_incompatible_qos = new rmw_offered_incompatible_qos_t();
+
+      rmw_offered_incompatible_qos.total_count = topic_writer_->total_count();
+      rmw_offered_incompatible_qos.total_count_change = topic_writer_->total_count_change();
+      //TODO
+      //rmw_offered_incompatible_qos.last_policy_id = topic_writer_->last_policy_id();
+      //rmw_offered_incompatible_qos.policies = topic_writer_->policies();
+
+      //todo return value?
+      //todo assign to void *?
+      return RMW_RET_ERROR;
+
+    case DDS_PUBLICATION_MATCHED:
+
+      PublicationMatchedStatus * publication_matched_status = topic_writer_->publication_matched_status();
+
+      rmw_publication_matched_t rmw_publication_matched = new rmw_publication_matched_t();
+      rmw_publication_matched_t.total_count = topic_writer_->total_count();
+      rmw_publication_matched_t.total_count_change = topic_writer_->total_count_change();
+      rmw_publication_matched_t.current_count = topic_writer_->current_count();
+      rmw_publication_matched_t.current_count_change = topic_writer_->current_count_change();
+      //TODO
+      //rmw_publication_matched_t.last_subscription_handle = topic_writer_->last_subscription_handle();
+
+      //todo return value?
+      //todo assign to void *?
+      break;
+
+    default:
+      //todo is this really ok?
+      return RMW_RET_OK;
+  }
+}
+inline DDSEntity* ConnextStaticSubscriberInfo::get_entity()
+{
+  return dds_publisher_;
+}
 
 #endif  // RMW_CONNEXT_CPP__CONNEXT_STATIC_PUBLISHER_INFO_HPP_
--- rmw_connext_cpp/include/rmw_connext_cpp/connext_static_subscriber_info.hpp
+++ rmw_connext_cpp/include/rmw_connext_cpp/connext_static_subscriber_info.hpp
@@ -19,6 +19,8 @@
 
 #include "rmw_connext_shared_cpp/ndds_include.hpp"
 
+#include "rmw_connext_shared_cpp/types.hpp"
+
 #include "rosidl_typesupport_connext_cpp/message_type_support.h"
 
 class ConnextSubscriberListener;
@@ -57,27 +59,114 @@ private:
   std::atomic<std::size_t> current_count_;
 };
 
-inline void ConnextStaticSubscriberInfo::get_status(
+/**
+ * Remap the specific RTI Connext DDS DataReader Status to a generic RMW status type.
+ *
+ * @param mask input status mask
+ * @param event
+ */
+inline rmw_ret_code ConnextStaticSubscriberInfo::get_status(
   const DDS_StatusMask mask,
   void * event)
 {
+
+  //DDS_StatusMask is a list of DDS_StatusKind
+  // (https://community.rti.com/rti-doc/500/ndds.5.0.0/doc/html/api_cpp/group__DDSStatusTypesModule.html#ga4d99a5cbe5e3451400717c8358be6377)
+  //todo: what about multiple status bits? should we be setting void * as a vector / array with all the relevant statuses?
   switch(mask) {
     case DDS_SAMPLE_REJECTED_STATUS:
+
+      SampleRejectedStatus * sample_rejected = topic_reader_->sample_rejected_status();
+
+      rmw_sample_rejected_status_t rmw_sample_rejected_status = new sample_rejected_status_t();
+      rmw_sample_rejected_status.total_count = sample_rejected->total_count();
+      rmw_sample_rejected_status.total_count_change = sample_rejected->total_count_change();
+      //TODO
+      //rmw_sample_rejected_status.last_reason = sample_rejected->last_reason();
+      //rmw_sample_rejected_status.last_instance_handle = sample_rejected->last_instance_handle();
+
+      //todo return value?
+      //todo assign to void *?
       break;
+
     case DDS_LIVELINESS_CHANGED_STATUS:
-      auto status = topic_reader_->liveliness_changed_status();
-      event << status;
+
+      LivelinessChangedStatus * liveliness_changed = topic_reader_->liveliness_changed_status();
+
+      rmw_liveliness_changed_status_t rmw_liveliness_changed_status = new liveliness_changed_status_t();
+      rmw_liveliness_changed_status.alive_count = liveliness_changed_status->alive_count();
+      rmw_liveliness_changed_status.not_alive_count = liveliness_changed_status->not_alive_count();
+      rmw_liveliness_changed_status.alive_count_change = liveliness_changed_status->alive_count_change();
+      rmw_liveliness_changed_status.not_alive_count_change = liveliness_changed_status->not_alive_count_change();
+      //TODO
+      //rmw_liveliness_changed_status.last_publication_handle = liveliness_changed_status->
+
+      //todo return value?
+      //todo assign to void *?
       break;
+
     case DDS_REQUESTED_DEADLINE_MISSED_STATUS:
+
+      LivelinessChangedStatus * liveliness_changed = topic_reader_->requested_deadline_missed_status();
+
+      rmw_liveliness_changed_status_t rmw_liveliness_changed_status = new rmw_liveliness_changed_status_t();
+      rmw_liveliness_changed_status.alive_count = liveliness_changed->alive_count();
+      rmw_liveliness_changed_status.not_alive_count = liveliness_changed->not_alive_count();
+      rmw_liveliness_changed_status.alive_count_change = liveliness_changed->alive_count_change();
+      rmw_liveliness_changed_status.not_alive_count_change = liveliness_changed->not_alive_count_change();
+      //TODO
+      //rmw_liveliness_changed_status.last_publication_handle = liveliness_changed->last_publication_handle();
+
+      //todo return value?
+      //todo assign to void *?
       break;
+
     case DDS_REQUESTED_INCOMPATIBLE_QOS_STATUS:
+
+      RequestedIncompatibleQosStatus * requested_incompatible = topic_reader_->requested_incompatible_qos_status();
+
+      rmw_requested_incompatible_qos_status_t rmw_requested_incompatible_qos_status = new rmw_requested_incompatible_qos_status_t();
+      rmw_requested_incompatible_qos_status.total_count = topic_reader_->total_count();
+      rmw_requested_incompatible_qos_status.total_count_change = topic_reader_->total_count_change();
+      //TODO
+      //rmw_requested_incompatible_qos_status.policies = topic_reader_->policies();
+      //rmw_requested_incompatible_qos_status.last_policy_id = topic_reader_->last_policy_id();
+
+      //todo return value?
+      //todo assign to void *?
       break;
+
     case DDS_SAMPLE_LOST_STATUS:
+
+      SampleLostStatus * sample_lost_status = topic_reader_->sample_lost_status();
+
+      rmw_sample_lost_status_t rmw_sample_lost_status = new sample_lost_status_t();
+      rmw_sample_lost_status.total_count-> status->total_count();
+      rmw_sample_lost_status.total_count_change->status->total_count_change();
+
+      //todo return value?
+      //todo assign to void *?
       break;
+
     case DDS_SUBSCRIPTION_MATCHED_STATUS:
+
+      SubscriptionMatchedStatus * subscription_matched = topic_reader_->subscription_matched_status();
+
+      rmw_subscription_matched_status_t rmw_subscription_matched_status = new rmw_subscription_matched_status_t();
+      rmw_subscription_matched_status.total_count = subscription_matched->total_count();
+      rmw_subscription_matched_status.total_count_change = subscription_matched->total_count_changed();
+      rmw_subscription_matched_status.current_count = subscription_matched->total_count();
+      rmw_subscription_matched_status.current_count_change = subscription_matched->total_count_changed();
+      //TODO
+      //rmw_subscription_matched_status.last_publication_handle = subscription_matched->last_publication_handle();
+
+      //todo return value?
+      //todo assign to void *?
       break;
+
     default:
-      return RMW_RET_ERROR;
+      //todo is this really ok?
+      return RMW_RET_OK;
   }
 }
 
