1c57b0c88eb1f53e8582a10370169c3a2a9e0fe5
Emerson Knapp <eknapp@amazon.com>
2019-03-21 13:18:34 -0700

    Very linty
    Signed-off-by: Emerson Knapp <eknapp@amazon.com>
    Signed-off-by: Devin Bonnie <dbbonnie@amazon.com>
    Signed-off-by: Burek <burekn@f45c89c6612b.ant.amazon.com>

--- rmw_connext_shared_cpp/src/event_converter.cpp
+++ rmw_connext_shared_cpp/src/event_converter.cpp
@@ -12,30 +12,31 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "event_converter.hpp"
+#include <unordered_map>
+
+#include "rmw_connext_shared_cpp/event_converter.hpp"
 
 
-namespace {
-    static const std::unordered_map<rmw_event_type_t, DDS_StatusMask> mask_map{
-      {RMW_EVENT_SAMPLE_REJECTED,            DDS_SAMPLE_REJECTED_STATUS},
-      {RMW_EVENT_LIVELINESS_CHANGED,         DDS_LIVELINESS_CHANGED_STATUS},
-      {RMW_EVENT_REQUESTED_DEADLINE_MISSED,  DDS_REQUESTED_DEADLINE_MISSED_STATUS},
-      {RMW_EVENT_REQUESTED_INCOMPATIBLE_QOS, DDS_REQUESTED_INCOMPATIBLE_QOS_STATUS},
-      {RMW_EVENT_DATA_AVAILABLE,             DDS_DATA_AVAILABLE_STATUS},
-      {RMW_EVENT_SAMPLE_LOST,                DDS_SAMPLE_LOST_STATUS},
-      {RMW_EVENT_SUBSCRIPTION_MATCHED,       DDS_SUBSCRIPTION_MATCHED_STATUS},
-      {RMW_EVENT_LIVELINESS_LOST,            DDS_LIVELINESS_LOST_STATUS},
-      {RMW_EVENT_OFFERED_DEADLINE_MISSED,    DDS_OFFERED_DEADLINE_MISSED_STATUS},
-      {RMW_EVENT_OFFERED_INCOMPATIBLE_QOS,   DDS_OFFERED_INCOMPATIBLE_QOS_STATUS},
-      {RMW_EVENT_PUBLICATION_MATCHED,        DDS_PUBLICATION_MATCHED_STATUS}
+static const std::unordered_map<rmw_event_type_t, DDS_StatusMask> mask_map{
+  {RMW_EVENT_SAMPLE_REJECTED, DDS_SAMPLE_REJECTED_STATUS},
+  {RMW_EVENT_LIVELINESS_CHANGED, DDS_LIVELINESS_CHANGED_STATUS},
+  {RMW_EVENT_REQUESTED_DEADLINE_MISSED, DDS_REQUESTED_DEADLINE_MISSED_STATUS},
+  {RMW_EVENT_REQUESTED_INCOMPATIBLE_QOS, DDS_REQUESTED_INCOMPATIBLE_QOS_STATUS},
+  {RMW_EVENT_DATA_AVAILABLE, DDS_DATA_AVAILABLE_STATUS},
+  {RMW_EVENT_SAMPLE_LOST, DDS_SAMPLE_LOST_STATUS},
+  {RMW_EVENT_SUBSCRIPTION_MATCHED, DDS_SUBSCRIPTION_MATCHED_STATUS},
+  {RMW_EVENT_LIVELINESS_LOST, DDS_LIVELINESS_LOST_STATUS},
+  {RMW_EVENT_OFFERED_DEADLINE_MISSED, DDS_OFFERED_DEADLINE_MISSED_STATUS},
+  {RMW_EVENT_OFFERED_INCOMPATIBLE_QOS, DDS_OFFERED_INCOMPATIBLE_QOS_STATUS},
+  {RMW_EVENT_PUBLICATION_MATCHED, DDS_PUBLICATION_MATCHED_STATUS}
+};
 
-    }};
-}
-
-DDS_StatusMask get_mask_from_rmw(const rmw_event_type_t & event_t) {
+DDS_StatusMask get_mask_from_rmw(const rmw_event_type_t & event_t)
+{
   return mask_map.at(event_t);
 }
 
-bool is_event_supported(const rmw_event_type_t &event_t) {
+bool is_event_supported(const rmw_event_type_t & event_t)
+{
   return mask_map.count(event_t) > 0;
 }
