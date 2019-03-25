bc05aa5bd3c8b7ab51e8325d9f4a5d817050cc69
Emerson Knapp <eknapp@amazon.com>
2019-03-25 12:46:51 -0700

    Set liveliness/deadline/lifespan QoS settings on Connext
    Signed-off-by: Emerson Knapp <eknapp@amazon.com>
    Signed-off-by: Devin Bonnie <dbbonnie@amazon.com>
    Signed-off-by: Burek <burekn@f45c89c6612b.ant.amazon.com>

--- rmw_connext_shared_cpp/src/qos.cpp
+++ rmw_connext_shared_cpp/src/qos.cpp
@@ -14,6 +14,127 @@
 
 #include "rmw_connext_shared_cpp/qos.hpp"
 
+namespace {
+
+template<typename DDSEntityQos>
+bool
+set_entity_qos_from_profile_generic(
+  const rmw_qos_profile_t & qos_profile,
+  DDSEntityQos & entity_qos)
+{
+  // Read properties from the rmw profile
+  switch (qos_profile.history) {
+    case RMW_QOS_POLICY_HISTORY_KEEP_LAST:
+      entity_qos.history.kind = DDS::KEEP_LAST_HISTORY_QOS;
+      break;
+    case RMW_QOS_POLICY_HISTORY_KEEP_ALL:
+      entity_qos.history.kind = DDS::KEEP_ALL_HISTORY_QOS;
+      break;
+    case RMW_QOS_POLICY_HISTORY_SYSTEM_DEFAULT:
+      break;
+    default:
+      RMW_SET_ERROR_MSG("Unknown QoS history policy");
+      return false;
+  }
+
+  switch (qos_profile.reliability) {
+    case RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT:
+      entity_qos.reliability.kind = DDS::BEST_EFFORT_RELIABILITY_QOS;
+      break;
+    case RMW_QOS_POLICY_RELIABILITY_RELIABLE:
+      entity_qos.reliability.kind = DDS::RELIABLE_RELIABILITY_QOS;
+      break;
+    case RMW_QOS_POLICY_RELIABILITY_SYSTEM_DEFAULT:
+      break;
+    default:
+      RMW_SET_ERROR_MSG("Unknown QoS reliability policy");
+      return false;
+  }
+
+  switch (qos_profile.durability) {
+    case RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL:
+      entity_qos.durability.kind = DDS::TRANSIENT_LOCAL_DURABILITY_QOS;
+      break;
+    case RMW_QOS_POLICY_DURABILITY_VOLATILE:
+      entity_qos.durability.kind = DDS::VOLATILE_DURABILITY_QOS;
+      break;
+    case RMW_QOS_POLICY_DURABILITY_SYSTEM_DEFAULT:
+      break;
+    default:
+      RMW_SET_ERROR_MSG("Unknown QoS durability policy");
+      return false;
+  }
+
+  if (qos_profile.depth != RMW_QOS_POLICY_DEPTH_SYSTEM_DEFAULT) {
+    entity_qos.history.depth = static_cast<DDS::Long>(qos_profile.depth);
+  }
+
+  if (qos_profile.deadline.sec != 0 || qos_profile.deadline.nsec != 0) {
+    entity_qos.deadline.period.sec = qos_profile.deadline.sec;
+    entity_qos.deadline.period.nanosec = qos_profile.deadline.nsec;
+  }
+
+  switch (qos_profile.liveliness) {
+    case RMW_QOS_POLICY_LIVELINESS_AUTOMATIC:
+      entity_qos.liveliness.kind = DDS::AUTOMATIC_LIVELINESS_QOS;
+      break;
+    case RMW_QOS_POLICY_LIVELINESS_MANUAL_BY_PARTICIPANT:
+      entity_qos.liveliness.kind = DDS::MANUAL_BY_PARTICIPANT_LIVELINESS_QOS;
+      break;
+    case RMW_QOS_POLICY_LIVELINESS_MANUAL_BY_TOPIC:
+      entity_qos.liveliness.kind = DDS::MANUAL_BY_TOPIC_LIVELINESS_QOS;
+      break;
+    case RMW_QOS_POLICY_LIVELINESS_SYSTEM_DEFAULT:
+      break;
+    default:
+      RMW_SET_ERROR_MSG("Unknown QoS liveliness policy");
+      return false;
+  }
+  if (qos_profile.liveliness_lease_duration.sec != 0 || qos_profile.liveliness_lease_duration.nsec != 0) {
+    entity_qos.liveliness.lease_duration.sec = qos_profile.liveliness_lease_duration.sec;
+    entity_qos.liveliness.lease_duration.nanosec = qos_profile.liveliness_lease_duration.nsec;
+  }
+
+  // ensure the history depth is at least the requested queue size
+  assert(entity_qos.history.depth >= 0);
+  if (
+    entity_qos.history.kind == DDS::KEEP_LAST_HISTORY_QOS &&
+    static_cast<size_t>(entity_qos.history.depth) < qos_profile.depth)
+  {
+    if (qos_profile.depth > (std::numeric_limits<DDS::Long>::max)()) {
+      RMW_SET_ERROR_MSG(
+        "failed to set history depth since the requested queue size exceeds the DDS type");
+      return false;
+    }
+    entity_qos.history.depth = static_cast<DDS::Long>(qos_profile.depth);
+  }
+  return true;
+}
+
+bool
+set_entity_qos_from_profile(
+  const rmw_qos_profile_t & qos_profile,
+  DDS::DataReaderQos& entity_qos)
+{
+  // Set any QoS settings that are specific to DataReader, then call the shared version
+  return set_entity_qos_from_profile_generic(qos_profile, entity_qos);
+}
+
+bool
+set_entity_qos_from_profile(
+  const rmw_qos_profile_t & qos_profile,
+  DDS::DataWriterQos& entity_qos)
+{
+  // Set any QoS settings that are specific to DataWriter, then call the shared version
+  if (qos_profile.lifespan.sec != 0 || qos_profile.lifespan.nsec != 0) {
+    entity_qos.lifespan.duration.sec = qos_profile.lifespan.sec;
+    entity_qos.lifespan.duration.nanosec = qos_profile.lifespan.nsec;
+  }
+  return set_entity_qos_from_profile_generic(qos_profile, entity_qos);
+}
+
+}  // anonymous namespace
+
 bool
 get_datareader_qos(
   DDS::DomainParticipant * participant,
