48d3365603196c2ca63a508fdf2cabcea5cee039
Emerson Knapp <eknapp@amazon.com>
2019-03-25 12:46:51 -0700

    Set liveliness/deadline/lifespan QoS settings on Connext
    Signed-off-by: Emerson Knapp <eknapp@amazon.com>

--- rmw_connext_shared_cpp/include/rmw_connext_shared_cpp/qos.hpp
+++ rmw_connext_shared_cpp/include/rmw_connext_shared_cpp/qos.hpp
@@ -39,73 +39,17 @@ get_datawriter_qos(
   const rmw_qos_profile_t & qos_profile,
   DDS::DataWriterQos & datawriter_qos);
 
-template<typename DDSEntityQos>
+
+RMW_CONNEXT_SHARED_CPP_PUBLIC
 bool
 set_entity_qos_from_profile(
   const rmw_qos_profile_t & qos_profile,
-  DDSEntityQos & entity_qos)
-{
-  // Read properties from the rmw profile
-  switch (qos_profile.history) {
-    case RMW_QOS_POLICY_HISTORY_KEEP_LAST:
-      entity_qos.history.kind = DDS::KEEP_LAST_HISTORY_QOS;
-      break;
-    case RMW_QOS_POLICY_HISTORY_KEEP_ALL:
-      entity_qos.history.kind = DDS::KEEP_ALL_HISTORY_QOS;
-      break;
-    case RMW_QOS_POLICY_HISTORY_SYSTEM_DEFAULT:
-      break;
-    default:
-      RMW_SET_ERROR_MSG("Unknown QoS history policy");
-      return false;
-  }
+  DDS::DataReaderQos& entity_qos);
 
-  switch (qos_profile.reliability) {
-    case RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT:
-      entity_qos.reliability.kind = DDS::BEST_EFFORT_RELIABILITY_QOS;
-      break;
-    case RMW_QOS_POLICY_RELIABILITY_RELIABLE:
-      entity_qos.reliability.kind = DDS::RELIABLE_RELIABILITY_QOS;
-      break;
-    case RMW_QOS_POLICY_RELIABILITY_SYSTEM_DEFAULT:
-      break;
-    default:
-      RMW_SET_ERROR_MSG("Unknown QoS reliability policy");
-      return false;
-  }
-
-  switch (qos_profile.durability) {
-    case RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL:
-      entity_qos.durability.kind = DDS::TRANSIENT_LOCAL_DURABILITY_QOS;
-      break;
-    case RMW_QOS_POLICY_DURABILITY_VOLATILE:
-      entity_qos.durability.kind = DDS::VOLATILE_DURABILITY_QOS;
-      break;
-    case RMW_QOS_POLICY_DURABILITY_SYSTEM_DEFAULT:
-      break;
-    default:
-      RMW_SET_ERROR_MSG("Unknown QoS durability policy");
-      return false;
-  }
-
-  if (qos_profile.depth != RMW_QOS_POLICY_DEPTH_SYSTEM_DEFAULT) {
-    entity_qos.history.depth = static_cast<DDS::Long>(qos_profile.depth);
-  }
-
-  // ensure the history depth is at least the requested queue size
-  assert(entity_qos.history.depth >= 0);
-  if (
-    entity_qos.history.kind == DDS::KEEP_LAST_HISTORY_QOS &&
-    static_cast<size_t>(entity_qos.history.depth) < qos_profile.depth)
-  {
-    if (qos_profile.depth > (std::numeric_limits<DDS::Long>::max)()) {
-      RMW_SET_ERROR_MSG(
-        "failed to set history depth since the requested queue size exceeds the DDS type");
-      return false;
-    }
-    entity_qos.history.depth = static_cast<DDS::Long>(qos_profile.depth);
-  }
-  return true;
-}
+RMW_CONNEXT_SHARED_CPP_PUBLIC
+bool
+set_entity_qos_from_profile(
+  const rmw_qos_profile_t & qos_profile,
+  DDS::DataWriterQos& entity_qos);
 
 #endif  // RMW_CONNEXT_SHARED_CPP__QOS_HPP_
