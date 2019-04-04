3e8977c58e58d120aa6d0ad81d75cf36f3c68492
Devin Bonnie <dbbonnie@amazon.com>
2019-04-04 09:57:13 -0700

    Fixed missing changes from rebasing
    Signed-off-by: Devin Bonnie <dbbonnie@amazon.com>
    Signed-off-by: Burek <burekn@f45c89c6612b.ant.amazon.com>

--- rmw_connext_cpp/include/rmw_connext_cpp/connext_static_subscriber_info.hpp
+++ rmw_connext_cpp/include/rmw_connext_cpp/connext_static_subscriber_info.hpp
@@ -19,14 +19,12 @@
 
 #include "rmw_connext_shared_cpp/ndds_include.hpp"
 #include "rmw_connext_shared_cpp/connext_static_event_info.hpp"
-
 #include "rmw_connext_shared_cpp/types.hpp"
 
 #include "ndds/ndds_cpp.h"
 #include "ndds/ndds_namespace_cpp.h"
 
 #include "rosidl_typesupport_connext_cpp/message_type_support.h"
-#include "rmw_connext_shared_cpp/connext_static_event_info.hpp"
 #include "rmw/types.h"
 #include "rmw/ret_types.h"
 
--- rmw_connext_shared_cpp/include/rmw_connext_shared_cpp/connext_static_event_info.hpp
+++ rmw_connext_shared_cpp/include/rmw_connext_shared_cpp/connext_static_event_info.hpp
@@ -32,26 +32,8 @@ typedef struct ConnextCustomEventInfo
    * @return
    */
   virtual rmw_ret_t get_status(const DDS_StatusMask mask, void * event) = 0;
-  virtual DDSEntity* get_entity() = 0;
-  /**
-    * Assign the input DDS return code to its corresponding RMW return code.
-    * @param dds_return_code input DDS return code
-    * @return to_return the corresponding rmw_ret_t that maps to the input DDS_ReturnCode_t. By
-    * default RMW_RET_ERROR is returned if no corresponding rmw_ret_t is not defined.
-    */
-  rmw_ret_t check_dds_ret_code(DDS_ReturnCode_t & dds_return_code)
-  {
-    switch (dds_return_code) {
-      case DDS_RETCODE_OK:
-        return RMW_RET_OK;
-      case DDS_RETCODE_ERROR:
-        return RMW_RET_ERROR;
-      case DDS_RETCODE_TIMEOUT:
-        return RMW_RET_TIMEOUT;
-      default:
-        return RMW_RET_ERROR;
-    }
-  }
+  virtual DDSEntity * get_entity() = 0;
+
 } ConnextCustomEventInfo;
 
 #endif  // RMW_CONNEXT_SHARED_CPP__CONNEXT_STATIC_EVENT_INFO_HPP_
