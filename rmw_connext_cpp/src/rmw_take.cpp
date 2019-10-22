06357cc9f4f9f72aaa3567183a3ed12b8a1fd02a
Karsten Knese <Karsten1987@users.noreply.github.com>
2019-10-21 20:46:33 -0700

    use return_loaned_message_from (#376)
    Signed-off-by: Karsten Knese <karsten@openrobotics.org>

--- rmw_connext_cpp/src/rmw_take.cpp
+++ rmw_connext_cpp/src/rmw_take.cpp
@@ -363,14 +363,15 @@ rmw_take_loaned_message_with_info(
 }
 
 rmw_ret_t
-rmw_release_loaned_message(
+rmw_return_loaned_message_from_subscription(
   const rmw_subscription_t * subscription,
   void * loaned_message)
 {
   (void) subscription;
   (void) loaned_message;
 
-  RMW_SET_ERROR_MSG("rmw_release_loaned_message not implemented for rmw_connext_cpp");
+  RMW_SET_ERROR_MSG(
+    "rmw_release_loaned_message not implemented for rmw_connext_cpp");
   return RMW_RET_UNSUPPORTED;
 }
 }  // extern "C"
--- rmw_connext_dynamic_cpp/src/functions.cpp
+++ rmw_connext_dynamic_cpp/src/functions.cpp
@@ -712,14 +712,15 @@ rmw_borrow_loaned_message(
 }
 
 rmw_ret_t
-rmw_return_loaned_message(
+rmw_return_loaned_message_from_publisher(
   const rmw_publisher_t * publisher,
   void * loaned_message)
 {
   (void) publisher;
   (void) loaned_message;
 
-  RMW_SET_ERROR_MSG("rmw_return_loaned_message not implemented for rmw_connext_dynamic_cpp");
+  RMW_SET_ERROR_MSG(
+    "rmw_return_loaned_message_from_publisher not implemented for rmw_connext_dynamic_cpp");
   return RMW_RET_OK;
 }
 
@@ -1625,14 +1626,15 @@ rmw_take_loaned_message_with_info(
 }
 
 rmw_ret_t
-rmw_release_loaned_message(
+rmw_return_loaned_message_from_subscription(
   const rmw_subscription_t * subscription,
   void * loaned_message)
 {
   (void) subscription;
   (void) loaned_message;
 
-  RMW_SET_ERROR_MSG("rmw_release_loaned_message not implemented for rmw_connext_dynamic_cpp");
+  RMW_SET_ERROR_MSG(
+    "rmw_return_loaned_message_from_subscription not implemented for rmw_connext_dynamic_cpp");
   return RMW_RET_ERROR;
 }
 
