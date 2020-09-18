d17020e2a71d66df94334e3a029c86b95db1d048
Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>
2020-09-18 18:23:56 -0300

    Some extra cool configuration environment variables
    Signed-off-by: Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>

--- rmw_connext_shared_cpp/src/init.cpp
+++ rmw_connext_shared_cpp/src/init.cpp
@@ -23,8 +23,11 @@
 
 static std::once_flag g_run_once_flag;
 
-/// Tri-state retcode used in `set_default_qos_library`.
-enum class SetDefaultLibraryRet {SET, NOT_SET, FAILED};
+static bool g_is_ros_qos_ignored = false;
+static bool g_does_node_profile_override = false;
+
+/// Tri-state retcode used in `set_default_qos_library` and `is_env_variable_set`.
+enum class TristateRetCode {SET, NOT_SET, FAILED};
 
 /// Set the default Connext qos profile library.
 /**
@@ -33,12 +36,12 @@ enum class SetDefaultLibraryRet {SET, NOT_SET, FAILED};
  * If only one user provided library is found, that library is set as the default one.
  * If not, not qos profile library is set.
  *
- * \return `SetDefaultLibraryRet::SET` if the default library was set, or
- * \return `SetDefaultLibraryRet::NOT_SET` if the default library was not set, or
- * \return `SetDefaultLibraryRet::FAILED` if failed to read the environment variable
+ * \return `TristateRetCode::SET` if the default library was set, or
+ * \return `TristateRetCode::NOT_SET` if the default library was not set, or
+ * \return `TristateRetCode::FAILED` if failed to read the environment variable
  *  or to set the default library.
  */
-static SetDefaultLibraryRet
+static TristateRetCode
 set_default_qos_library(DDS::DomainParticipantFactory * dpf);
 
 /// Set the default Connext qos profile.
@@ -51,6 +54,17 @@ set_default_qos_library(DDS::DomainParticipantFactory * dpf);
  */
 static bool
 set_default_qos_profile(DDS::DomainParticipantFactory * dpf);
+
+/// Check if environment variable is set.
+/**
+ * Check if the provided environment variable is "1".
+ *
+ * \return `TristateRetCode::SET` if the environment variable of the provided name is equal to "1".
+ * \return `TristateRetCode::NOT_SET` if the environment variable has another value or is unset, or
+ * \return `TristateRetCode::FAILED` if failed to read the environment variable.
+ */
+static TristateRetCode
+is_env_variable_set(const char * env_var_name);
 
 rmw_ret_t
 init()
@@ -71,22 +85,49 @@ init()
       factory_qos.resource_limits.max_objects_per_thread = 8192;
       dpf->set_qos(factory_qos);
 
-      auto set_library_ret = set_default_qos_library(dpf);
-      if (SetDefaultLibraryRet::FAILED == set_library_ret) {
-        ret = RMW_RET_ERROR;
-        return;
-      }
-      if (SetDefaultLibraryRet::SET == set_library_ret) {
-        if (!set_default_qos_profile(dpf)) {
+      switch (set_default_qos_library(dpf)) {
+        case TristateRetCode::SET:
+          if (!set_default_qos_profile(dpf)) {
+            ret = RMW_RET_ERROR;
+          }
+          break;
+        case TristateRetCode::NOT_SET:
+          break;
+        default:  // fallthrough
+        case TristateRetCode::FAILED:
           ret = RMW_RET_ERROR;
-        }
+          return;
+      }
+
+      switch (is_env_variable_set("RMW_CONNEXT_IGNORE_ROS_QOS")) {
+        case TristateRetCode::SET:
+          g_is_ros_qos_ignored = true;
+          break;
+        case TristateRetCode::NOT_SET:
+          break;
+        default:  // fallthrough
+        case TristateRetCode::FAILED:
+          ret = RMW_RET_ERROR;
+          return;
+      }
+
+      switch (is_env_variable_set("RMW_CONNEXT_NODE_QOS_PROFILE_OVERRIDE")) {
+        case TristateRetCode::SET:
+          g_does_node_profile_override = true;
+          break;
+        case TristateRetCode::NOT_SET:
+          break;
+        default:  // fallthrough
+        case TristateRetCode::FAILED:
+          ret = RMW_RET_ERROR;
+          return;
       }
     }
   );
   return ret;
 }
 
-static SetDefaultLibraryRet
+static TristateRetCode
 set_default_qos_library(DDS::DomainParticipantFactory * dpf)
 {
   const char * qos_profile_library_name = NULL;
@@ -94,7 +135,7 @@ set_default_qos_library(DDS::DomainParticipantFactory * dpf)
     "RMW_CONNEXT_QOS_PROFILE_LIBRARY", &qos_profile_library_name);
   if (error) {
     RMW_SET_ERROR_MSG_WITH_FORMAT_STRING("rcutils_get_env() failed: '%s'", error);
-    return SetDefaultLibraryRet::FAILED;
+    return TristateRetCode::FAILED;
   }
   if (qos_profile_library_name && 0 == strcmp("", qos_profile_library_name)) {
     qos_profile_library_name = NULL;
@@ -105,11 +146,11 @@ set_default_qos_library(DDS::DomainParticipantFactory * dpf)
     // environment variable is empty
     if (DDS::RETCODE_OK != dpf->get_qos_profile_libraries(qos_libraries)) {
       RMW_SET_ERROR_MSG("failed to get qos profile libraries");
-      return SetDefaultLibraryRet::FAILED;
+      return TristateRetCode::FAILED;
     }
     // If only one non-builtin qos profile library was loaded, use that one.
     if (qos_libraries.length() > 3) {
-      return SetDefaultLibraryRet::NOT_SET;
+      return TristateRetCode::NOT_SET;
     }
     for (int i = 0; i < qos_libraries.length(); i++) {
       if (
@@ -129,11 +170,11 @@ set_default_qos_library(DDS::DomainParticipantFactory * dpf)
     if (DDS::RETCODE_OK != dpf->set_default_library(qos_profile_library_name)) {
       RMW_SET_ERROR_MSG_WITH_FORMAT_STRING(
         "failed to set default library \"%s\"", qos_profile_library_name);
-      return SetDefaultLibraryRet::FAILED;
+      return TristateRetCode::FAILED;
     }
-    return SetDefaultLibraryRet::SET;
+    return TristateRetCode::SET;
   }
-  return SetDefaultLibraryRet::NOT_SET;
+  return TristateRetCode::NOT_SET;
 }
 
 static bool
@@ -158,4 +199,32 @@ set_default_qos_profile(DDS::DomainParticipantFactory * dpf)
     return false;
   }
   return true;
+}
+
+static TristateRetCode
+is_env_variable_set(const char * env_var_name)
+{
+  const char * env_var_value = NULL;
+  const char * error = rcutils_get_env(
+    env_var_name, &env_var_value);
+  if (error) {
+    RMW_SET_ERROR_MSG_WITH_FORMAT_STRING("rcutils_get_env() failed: '%s'", error);
+    return TristateRetCode::FAILED;
+  }
+  if (env_var_value && 0 == strcmp("1", env_var_value)) {
+    return TristateRetCode::SET;
+  }
+  return TristateRetCode::NOT_SET;
+}
+
+bool
+is_ros_qos_ignored()
+{
+  return g_is_ros_qos_ignored;
+}
+
+bool
+does_node_profile_override()
+{
+  return g_does_node_profile_override;
 }
