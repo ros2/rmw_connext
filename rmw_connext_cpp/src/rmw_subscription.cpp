1b19ef896ee9156bbb332785ea697bd3f8e1e24f
Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>
2020-09-18 19:16:06 -0300

    Fix wrong error messages (#458)
    Signed-off-by: Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>

--- rmw_connext_cpp/src/rmw_subscription.cpp
+++ rmw_connext_cpp/src/rmw_subscription.cpp
@@ -254,7 +254,7 @@ rmw_create_subscription(
   subscription->topic_name = reinterpret_cast<const char *>(
     rmw_allocate(strlen(topic_name) + 1));
   if (!subscription->topic_name) {
-    RMW_SET_ERROR_MSG("failed to allocate memory for node name");
+    RMW_SET_ERROR_MSG("failed to allocate memory for topic name");
     goto fail;
   }
   memcpy(const_cast<char *>(subscription->topic_name), topic_name, strlen(topic_name) + 1);
