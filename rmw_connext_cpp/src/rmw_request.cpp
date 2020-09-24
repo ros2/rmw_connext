74dea3dbc36c2f049258732497b1ce7e8512dfb8
lobotuerk <jtlorente@ekumenlabs.com>
2020-09-24 18:03:41 -0300

    ccplint
    Signed-off-by: lobotuerk <jtlorente@ekumenlabs.com>

--- rmw_connext_cpp/src/rmw_request.cpp
+++ rmw_connext_cpp/src/rmw_request.cpp
@@ -33,7 +33,7 @@ rmw_send_request(
   RMW_CHECK_TYPE_IDENTIFIERS_MATCH(
     client handle,
     client->implementation_identifier, rti_connext_identifier,
-    return RMW_RET_INCORRECT_RMW_IMPLEMENTATION)
+    return RMW_RET_INCORRECT_RMW_IMPLEMENTATION);
 
   RMW_CHECK_ARGUMENT_FOR_NULL(ros_request, RMW_RET_INVALID_ARGUMENT);
 
